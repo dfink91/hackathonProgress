13c6363c7dfc070bc8d839f9f2cf1b2c80b18915
Danilo Fink <dfink91@gmail.com>
2022-11-12 11:54:02 +0100

    check concrete is now a func that takes in and output strings

--- check-concrete-correctness.py
+++ check-concrete-correctness.py
@@ -1,35 +1,41 @@
 import json
 
-inputFilename = 'sample_data_1.json'
-outputFilename = 'outputFormat.json'
+def checkStackCorrectness(inputStr, outputStr):
 
-inputFile = open(inputFilename)
-outputFile = open(outputFilename)
-input = json.load(inputFile)
-output = json.load(outputFile)
-
-
-inputElements = {}
-for e in input['elements']:
-  inputElements[e['id']] = e
+    input = json.loads(inputStr)
+    output = json.loads(outputStr)
+    inputElements = {}
+    for e in input['elements']:
+        inputElements[e['id']] = e
 
 
-ok = True
-for bed in output['beds']:
-    firstElement = bed['elements'][0]
-    concrete = inputElements[firstElement['id']]['concrete']
-    for bedEl in bed['elements']:
-        ok = inputElements[bedEl['id']]['concrete'] == concrete
+    ok = True
+    for bed in output['beds']:
+        firstElement = bed['elements'][0]
+        concrete = inputElements[firstElement['id']]['concrete']
+        for bedEl in bed['elements']:
+            ok = inputElements[bedEl['id']]['concrete'] == concrete
+            if (ok != True):
+                break
         if (ok != True):
             break
-    if (ok != True):
-        break
 
-if (ok):
+    return ok
+
+
+
+inputFilename = 'sample_data_1.json'
+outputFilename = 'output1ConcreteOk.json'
+
+inputStr = ""
+with open(inputFilename, 'r') as f:
+    inputStr = f.read()
+
+outputStr = ""
+with open(outputFilename, 'r') as f:
+    outputStr = f.read()
+
+if (checkStackCorrectness(inputStr, outputStr)):
     print(outputFilename + ": concrete is ok")
 else:
     print(outputFilename + ": concrete is not ok")
-
-
-inputFile.close
-outputFile.close
